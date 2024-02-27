abc752c67e217beb235fe68aca149edf6f82bb65
Eric <eric.coimbra@gmail.com>
2024-02-27 13:40:44 +0000

    added auto play for image carousel

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -4,18 +4,20 @@ import { Center } from '@mantine/core';
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import carouselStyles from './styles/Carousel.module.css';
+import { useRef } from 'react';
+import Autoplay from 'embla-carousel-autoplay';
 
 const ImageCarousel = ({ thumbnail, shinyThumbnail }: PokemonProperties) => {
   const [initialSlide, setInitialSlide] = useState(0);
   const { pathname } = useLocation();
+  const autoplay = useRef(Autoplay({ delay: 3000 }));
 
   useEffect(() => {
     setInitialSlide(0);
   }, [pathname]);
-  console.log(initialSlide);
   return (
     <Center>
-      <Carousel loop className={carouselStyles.caroussel} initialSlide={initialSlide}>
+      <Carousel loop className={carouselStyles.caroussel} initialSlide={initialSlide} plugins={[autoplay.current]}>
         <Carousel.Slide className={carouselStyles.pkmImgBox}>
           <img src={thumbnail} className={carouselStyles.pkmImg} />
           <div>Regular</div>
