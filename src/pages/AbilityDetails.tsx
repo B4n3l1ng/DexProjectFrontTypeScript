513dca735e407c5e3f4411208fe5e42b42231c25
Eric <eric.coimbra@gmail.com>
2024-02-27 14:55:29 +0000

    removed unused import

--- src/pages/AbilityDetails.tsx
+++ src/pages/AbilityDetails.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { AbilityProperties, PokemonProperties } from '../interfaces';
-import { Center, Loader } from '@mantine/core';
+import { Loader } from '@mantine/core';
 import Card from '../components/Card';
 import dexStyles from './styles/Pokedex.module.css';
 import detailStyles from './styles/AbilityDetails.module.css';
@@ -13,29 +13,28 @@ const AbilityDetails = () => {
   const [ability, setAbility] = useState<AbilityProperties>();
   const [pokemon, setPokemon] = useState<PokemonProperties[]>([]);
 
-  const fetchAbility = async () => {
-    try {
-      const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/abilities/${abilityId}`);
-      if (response.status === (200 || 304)) {
-        setAbility(response.data);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const fetchPokemonWithAbility = async () => {
-    try {
-      const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/abilities/${abilityId}/pokemon`);
-      if (response.status === (200 || 304)) {
-        setPokemon(response.data);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   useEffect(() => {
+    const fetchAbility = async () => {
+      try {
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/abilities/${abilityId}`);
+        if (response.status === (200 || 304)) {
+          setAbility(response.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const fetchPokemonWithAbility = async () => {
+      try {
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/abilities/${abilityId}/pokemon`);
+        if (response.status === (200 || 304)) {
+          setPokemon(response.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
     fetchAbility();
     fetchPokemonWithAbility();
   }, [abilityId]);
