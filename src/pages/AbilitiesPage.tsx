3667855ea659fc14db6ef10a7e4885651d8b9364
Eric <eric.coimbra@gmail.com>
2024-02-29 14:14:21 +0000

    pagination for all display pages

--- src/pages/AbilitiesPage.tsx
+++ src/pages/AbilitiesPage.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
-import { Loader } from '@mantine/core';
+import { Loader, Pagination } from '@mantine/core';
 import Card from '../components/Card';
 import { AbilityProperties } from '../interfaces';
 import abilitiesPageStyles from './styles/AbilitiesPage.module.css';
@@ -8,13 +8,14 @@ import abilitiesPageStyles from './styles/AbilitiesPage.module.css';
 const AbilitiesPage = () => {
   const [data, setData] = useState<AbilityProperties[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [display, setDisplay] = useState<AbilityProperties[]>();
+  const [activePage, setActivePage] = useState(1);
 
   const fetchAbilities = async () => {
     try {
       const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/abilities`);
       if (response.status === 200 || response.status === 304) {
         setData(response.data);
-        setIsLoading(false);
       }
     } catch (error) {
       console.log(error);
@@ -25,17 +26,40 @@ const AbilitiesPage = () => {
     fetchAbilities();
   }, []);
 
+  useEffect(() => {
+    if (data.length > 0) {
+      const copy = JSON.parse(JSON.stringify(data));
+      const newData = copy.slice((activePage - 1) * 50, activePage * 50);
+      setDisplay(newData);
+      setIsLoading(false);
+    }
+  }, [activePage, data]);
+
   return (
     <section className={abilitiesPageStyles.abilitiesPage}>
       <h1>All Abilities</h1>
       {isLoading ? (
         <Loader color="teal" size="lg" type="dots" className="loader" />
       ) : (
-        <div className={abilitiesPageStyles.list}>
-          {data!.map((ability) => (
-            <Card data={ability} type="ability" key={ability._id} />
-          ))}
-        </div>
+        <>
+          <Pagination
+            total={Math.ceil(data.length / 50)}
+            value={activePage}
+            onChange={(page) => {
+              setIsLoading(true);
+              setActivePage(page);
+            }}
+            mt="sm"
+            size="lg"
+            radius="lg"
+            color="teal"
+          />
+          <div className={abilitiesPageStyles.list}>
+            {display!.map((ability) => (
+              <Card data={ability} type="ability" key={ability._id} />
+            ))}
+          </div>
+        </>
       )}
     </section>
   );
--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -2,12 +2,14 @@ import { useState, useEffect } from 'react';
 import { ItemProperties } from '../interfaces';
 import axios from 'axios';
 import Card from '../components/Card';
-import { Loader } from '@mantine/core';
+import { Loader, Pagination } from '@mantine/core';
 import itemStyles from './styles/ItemsPage.module.css';
 
 const ItemsPage = () => {
   const [items, setItems] = useState<ItemProperties[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [activePage, setActivePage] = useState<number>(1);
+  const [display, setDisplay] = useState<ItemProperties[]>([]);
 
   useEffect(() => {
     const fetchItems = async () => {
@@ -16,7 +18,6 @@ const ItemsPage = () => {
         if (response.status === 200 || response.status === 304) {
           console.log(response.data);
           setItems(response.data);
-          setIsLoading(false);
         }
       } catch (error) {
         console.log(error);
@@ -25,17 +26,40 @@ const ItemsPage = () => {
     fetchItems();
   }, []);
 
+  useEffect(() => {
+    if (items.length > 0) {
+      const copy = JSON.parse(JSON.stringify(items));
+      const newData = copy.slice((activePage - 1) * 50, activePage * 50);
+      setDisplay(newData);
+      setIsLoading(false);
+    }
+  }, [activePage, items]);
+
   return (
     <section className={itemStyles.itemsPage}>
       <h1>All Items</h1>
       {isLoading ? (
         <Loader color="teal" size="lg" type="dots" className="loader" />
       ) : (
-        <div className={itemStyles.list}>
-          {items!.map((item) => (
-            <Card data={item} type="items" key={item._id} />
-          ))}
-        </div>
+        <>
+          <Pagination
+            total={Math.ceil(items.length / 50)}
+            value={activePage}
+            onChange={(page) => {
+              setIsLoading(true);
+              setActivePage(page);
+            }}
+            mt="sm"
+            size="lg"
+            radius="lg"
+            color="teal"
+          />
+          <div className={itemStyles.list}>
+            {display!.map((item) => (
+              <Card data={item} type="items" key={item._id} />
+            ))}
+          </div>
+        </>
       )}
     </section>
   );
